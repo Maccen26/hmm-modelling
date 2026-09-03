9442183dea7946a57ce91c0725dd6f3f72bace2d
Maccen26 <s234936@dtu.dk>
2026-09-03 13:41:40 +0200

    Copied plot and test statistics data from previous models

---
+++ drivers/plots/main_plots.py
@@ -0,0 +1,22 @@
+from drivers.utils import plot_hmm_diagnostics, load_model
+import matplotlib.pyplot as plt
+
+
+def main_plots():
+    model_names = [
+        "ordinary_hmm",
+        "ar_hmm",
+        "ar_2_hmm",
+        "second_order_hmm",
+        "ar_2_second_order_hmm",
+        "covariate_hmm",
+    ]
+
+    for model_name in model_names:
+        model = load_model(f"results/models/{model_name}.pkl")
+        fig = plot_hmm_diagnostics(model, save_path=f"results/plots/{model_name}_diagnostics.png")
+        plt.close(fig)
+
+
+if __name__ == "__main__":
+    main_plots()
