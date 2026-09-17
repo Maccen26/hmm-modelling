b3e5cadc9e8be24d88909de777dca605775e8e9c
Maccen26 <s234936@dtu.dk>
2026-09-17 13:01:47 +0200

    Refactored classes

--- src/api/v4/__init__.py
+++ src/api/v4/__init__.py
@@ -1,9 +1,9 @@
-from src.api.v4.transitions import StaticTransition, StaticTransitionHigherOrder, DynamicTransition
+from src.api.v4.transitions import StaticTransition, StaticTransitionHigherOrder, DynamicTransition, ContinuousStaticTransition
 from src.api.v4.emissions import GaussEmission, AutoregressiveGaussEmission
 from src.api.v4.hmm_models import HMMParams, HMM
 from src.api.v4.algorithms import ForwardAlgorithm
 from src.api.v4.algorithms import ForwardOutput
-from src.api.v4.solvers import GradientSolver, LBFGSSolver, Minimizer
+from src.api.v4.solvers import GradientSolver, LBFGSSolver
 from src.api.v4.hmm_models import AIC, BIC
 
 __all__ = [
@@ -16,9 +16,9 @@ __all__ = [
     "ForwardOutput",
     "GradientSolver",
     "LBFGSSolver",
-    "Minimizer",
-    "StaticTransitionHigherOrder", 
+    "StaticTransitionHigherOrder",
     "AIC",
     "BIC", 
-    "DynamicTransition"
+    "DynamicTransition", 
+    "ContinuousStaticTransition"
 ]
